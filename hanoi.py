550d93b5ac4d5809c932b1d6ff898237ccf314ac
Brielle Veale <bsvea600@gmail.com>
2020-11-23 15:34:15 -0500

    made changes to hanoi new version

--- hanoi.py
+++ hanoi.py
@@ -1,25 +1,24 @@
-	def move(f,t) :
-	print("Move disc from {} to {} !".format(f,t))
+def move(f,t) :
+	print("Move disc from {} to {} !".format(f,t)) #.format is a method
 
-	move("A", "C")
-	Move disc from A to C:
+def moveVia(f,v,t):
+	move(f,v)  # function call to move(): move from step one to step two
+	# at first , the destination is v
+	move(v,t) # move from step 2 to step 3 (out of 3)
+		# now, the origin is v, an the destination is t
 
-	def moveVin(f,v,t) :
-		move(f,v)
-		move(v,t)
+	def hanoi(n,f,h,t):	
+		# n - number of discs
+		# f - fromt position
+ 		# h - helper position
+		# t - target position
+		if n == 0:
+			pass # this is the BASE CASE!
+		else:
+			hanoi(n-1,f,t,h) # function call within the function definition! (the recursion!)
+			move(f,t)
+			hanoi(n-1,h,f,t)
 
-	moveVin("A", "B", "C")
-	Move disc from A to B:
-	Move disc from B to C:
+	hanoi(4,"A","B","C") # the function call that gets things going!
 
-	def foo(x) :
-		foo(x)
-
-	foo(3)
-
-	def hanoi(n,f,h,t) :
-		hanoi(n-1,f,t,h)
-		move(f,t)
-		hanoi(n-1,h,f,t)
-
-	hanoi(4,"A","B","C")
+	# based on a tutorial by Prof. Thorsten Altenkirch
